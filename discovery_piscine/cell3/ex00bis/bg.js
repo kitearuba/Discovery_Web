5c8a75253ab68d71a419a209cdd25b644e9c4a42
Christian <115067566+kitearuba@users.noreply.github.com>
2024-02-23 13:12:08 +0100

    Update bg.js to jquery

--- discovery_piscine/cell3/ex00bis/bg.js
+++ discovery_piscine/cell3/ex00bis/bg.js
@@ -4,3 +4,11 @@ function cambiarColor() {
     let color3 = Math.floor(Math.random() * 256);
     document.body.style.background = "rgb(" + color1 + ", " + color2 + ", " + color3 + ")";
 }
+
+
+function cambiarColor() {
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+    $('body').css('background', 'rgb(' + color1 + ', ' + color2 + ', ' + color3 + ')');
+}
