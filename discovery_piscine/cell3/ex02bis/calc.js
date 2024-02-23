74acefd77eebd6661b0b73073dd12175ee9b7d99
Christian <115067566+kitearuba@users.noreply.github.com>
2024-02-23 13:16:50 +0100

    Update calc.js jquery conversion

--- discovery_piscine/cell3/ex02bis/calc.js
+++ discovery_piscine/cell3/ex02bis/calc.js
@@ -56,3 +56,43 @@ document.addEventListener('DOMContentLoaded', function() {
         alert("Please, use me...");
     }, 30000);
 });
+
+
+$(document).ready(function() {
+    $('#calcForm').submit(function(event) {
+        event.preventDefault();
+
+        const miembroIzquierdo = parseFloat($('#miembroIzquierdo').val());
+        const operador = $('#operador').val();
+        const miembroDerecho = parseFloat($('#miembroDerecho').val());
+
+        if (isNaN(miembroIzquierdo) || isNaN(miembroDerecho) || miembroIzquierdo < 0 || miembroDerecho < 0) {
+            alert("Error :(");
+            console.log("Error :(");
+            return;
+        }
+
+        let resultado;
+
+        switch (operador) {
+            case '+':
+                resultado = miembroIzquierdo + miembroDerecho;
+                break;
+            case '-':
+                resultado = miembroIzquierdo - miembroDerecho;
+                break;
+            case '*':
+                resultado = miembroIzquierdo * miembroDerecho;
+                break;
+            case '/':
+                if (miembroDerecho === 0) {
+                    alert("It's over 9000!");
+                    console.log("It's over 9000!");
+                    return;
+                }
+                resultado = miembroIzquierdo / miembroDerecho;
+                break;
+            case '%':
+                if (miembroDerecho === 0) {
+                    alert("It's over ⬤
+
